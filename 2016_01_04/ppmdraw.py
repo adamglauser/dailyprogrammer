b46811b727b6a686a76aec8131d07456a7702570
Adam Glauser <adamglauser@gmail.com>
2016-01-06 13:44:04 -0500

    Support lines of length 0 and 1

--- 2016_01_04/ppmdraw.py
+++ 2016_01_04/ppmdraw.py
@@ -9,6 +9,11 @@ class Coordinate(object):
 		self.x = x
 		self.y = y
 
+	def __eq__(self, other):
+		if type(other) is type(self):
+			return other.__dict__ == self.__dict__
+		return False
+
 class Colour(object):
 	def __init__(self, r, g, b):
 		self.r = r
@@ -30,6 +35,30 @@ class Point(Shape):
 	def draw(self, canvas):
 		canvas.setPoint(self.origin, self.colour)
 
+class Line(Shape):
+	def __init__(self, origin, end, colour):
+		super(Line, self).__init__(origin, colour)
+		self.end = end
+
+	def draw(self, canvas):
+		if (self.end.x == self.origin.x):
+			slope = 1
+		else:
+			slope = (self.end.y - self.origin.y) / (self.end.x - self.origin.x)
+		self.drawBySlope(canvas, slope, self.origin)
+
+	def drawBySlope(self, canvas, slope, startPoint):
+		if (startPoint == self.end):
+			canvas.setPoint(startPoint, self.colour)
+			return
+
+		# if the startPoint is adjacent to the end point, we can just
+		#  fill it in then draw a line on the end point
+		if (Canvas.areAdjacent(startPoint, self.end)):
+			canvas.setPoint(startPoint, self.colour)
+			self.drawBySlope(canvas, slope, self.end)
+			return
+
 class Canvas(object):
 	def __init__(self, col, row):
 		self.grid = [[Colour(0,0,0) for x in range(col)] for x in range(row)]
@@ -44,8 +73,27 @@ class Canvas(object):
 				print('   ', end="")
 			print('')
 
+	@staticmethod
+	def areAdjacent(p1, p2):
+		if not(isinstance(p1, Coordinate)) or not(isinstance(p2, Coordinate)):
+			return False
+		if abs(p1.x - p2.x) <= 1 and abs(p1.y - p2.y) <= 1:
+			return True
+		return False
+
 if __name__ == '__main__':
-	canvas = Canvas(2,2)
+	canvas = Canvas(3,3)
 	testPoint = Point(Coordinate(0,1),Colour(255,0,0))
 	testPoint.draw(canvas)
+	canvas.show()
+	print('')
+	print('')
+
+	testLine = (Line(Coordinate(1,1),Coordinate(1,1),Colour(0,255,0)),Line(Coordinate(2,0),Coordinate(2,1),Colour(0,0,255)))
+	testLine[0].draw(canvas)
+	canvas.show()
+	print('')
+	print('')
+
+	testLine[1].draw(canvas)
 	canvas.show()
