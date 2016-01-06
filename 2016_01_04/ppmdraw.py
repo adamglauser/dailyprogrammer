f5cef4e49ae09b3a29e69ddf617a1fcea7af3f82
Adam Glauser <adamglauser@gmail.com>
2016-01-06 18:43:40 -0500

    Support arbitrary lines

--- 2016_01_04/ppmdraw.py
+++ 2016_01_04/ppmdraw.py
@@ -3,6 +3,7 @@
 # The task is to read a file with instructions for creating various shapes
 # on a canvas. The output should be a .ppm image file containing those shapes
 # on a black background.
+import pdb
 
 class Coordinate(object):
 	def __init__(self, x, y):
@@ -41,13 +42,11 @@ class Line(Shape):
 		self.end = end
 
 	def draw(self, canvas):
-		if (self.end.x == self.origin.x):
-			slope = 1 if self.origin.y <= self.end.y else -1
-		else:
-			slope = (self.end.y - self.origin.y) / (self.end.x - self.origin.x)
-		self.drawBySlope(canvas, slope, self.origin)
+		self.drawBySlope(canvas, None, self.origin)
 
 	def drawBySlope(self, canvas, slope, startPoint):
+		canvas.setPoint(startPoint, self.colour)
+		slope = Line.getSlope(self.origin, self.end)
 		if (startPoint == self.end):
 			canvas.setPoint(startPoint, self.colour)
 			return
@@ -55,20 +54,60 @@ class Line(Shape):
 		# if the startPoint is adjacent to the end point, we can just
 		#  fill it in then draw a line on the end point
 		if (Canvas.areAdjacent(startPoint, self.end)):
-			canvas.setPoint(startPoint, self.colour)
 			self.drawBySlope(canvas, slope, self.end)
 			return
 
 		# horizontal and vertical lines are easy
 		if (slope == 0):
-			canvas.setPoint(startPoint, self.colour)
 			direction = 1 if startPoint.x <= self.end.x else -1
 			nextPoint = Coordinate(startPoint.x + direction, startPoint.y)
 			self.drawBySlope(canvas, slope, nextPoint)
-		if (abs(slope) == 1):
-			canvas.setPoint(startPoint, self.colour)
-			nextPoint = Coordinate(startPoint.x, startPoint.y + slope) 
+			return
+		if (slope == None):
+			yDirection = 1 if startPoint.y <= self.end.y else -1
+			nextPoint = Coordinate(startPoint.x, startPoint.y + yDirection) 
 			self.drawBySlope(canvas, slope, nextPoint)
+			return
+
+
+		# depending on the quadrant the line is in, only three possible points could be next on the line
+		# figure out which three depending on line direction, then choose the one that will result in a line
+		# with the slope closest to the final line
+		xDirection = 1 if startPoint.x <= self.end.x else -1
+		yDirection = 1 if startPoint.y <= self.end.y else -1
+		candidate = (Coordinate(startPoint.x, startPoint.y + yDirection),Coordinate(startPoint.x + xDirection, startPoint.y),Coordinate(startPoint.x + xDirection, startPoint.y + yDirection))
+
+		closestSlope = None
+		closestCandidateIndex = None
+		newSlope = [0, 0, 0] 
+		for i in range(len(candidate)):
+			newSlope[i] = Line.getSlope(self.origin, candidate[i])
+			if (closestSlope is None or abs(newSlope[i] - slope) < abs(closestSlope - slope)):
+				closestSlope = newSlope[i]
+				closestCandidateIndex = i
+
+		# in case my algorithm doesn't work well, to avoid infinite loop
+		if ((xDirection > 0 and candidate[closestCandidateIndex].x > self.end.x) 
+				or (xDirection < 0 and candidate[closestCandidateIndex].x < self.end.x) 
+				or (yDirection > 0 and candidate[closestCandidateIndex].y > self.end.y) 
+				or (yDirection < 0 and candidate[closestCandidateIndex].y < self.end.y)
+				):
+			self.drawBySlope(canvas, slope, self.end)
+		else:
+			self.drawBySlope(canvas, slope, candidate[closestCandidateIndex])
+		
+	@staticmethod
+	def getSlope(p1, p2):
+		if not(isinstance(p1, Coordinate)) or not(isinstance(p2, Coordinate)):
+			#error
+			return None
+
+		if (p2.x == p1.x):
+			slope = None 
+		else:
+			slope = (p2.y - p1.y) / (p2.x - p1.x)
+
+		return slope
 
 class Canvas(object):
 	def __init__(self, col, row):
@@ -113,6 +152,35 @@ if __name__ == '__main__':
 
 	canvas = Canvas(7, 7)
 	testLine = (Line(Coordinate(1,1),Coordinate(1,6),Colour(0,255,0)),Line(Coordinate(0,2),Coordinate(6,2),Colour(0,0,255)),Line(Coordinate(5,4),Coordinate(1,4),Colour(255,0,0)),Line(Coordinate(5,6),Coordinate(5,1),Colour(255,255,0)))
+	testLine[0].draw(canvas)
+	canvas.show()
+	print('')
+	print('')
+
+	testLine[1].draw(canvas)
+	canvas.show()
+	print('')
+	print('')
+
+	testLine[2].draw(canvas)
+	canvas.show()
+	print('')
+	print('')
+
+	#pdb.set_trace()
+	testLine[3].draw(canvas)
+	canvas.show()
+	print('')
+	print('')
+
+	print('Test diagonal lines')
+	canvas = Canvas(20, 20)
+	testLine = (
+		Line(Coordinate(0,0),Coordinate(19,19),Colour(0,255,0)),
+		Line(Coordinate(1,0),Coordinate(17,2),Colour(0,0,255)),
+		Line(Coordinate(15,14),Coordinate(3,4),Colour(255,0,0)),
+		Line(Coordinate(5,6),Coordinate(0,1),Colour(255,255,0)))
+	#pdb.set_trace()
 	testLine[0].draw(canvas)
 	canvas.show()
 	print('')
